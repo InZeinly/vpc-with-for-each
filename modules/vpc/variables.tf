4c04b32f688ddef1ca62d22b1ae79ab1fc286d26
InZeinly <tuzar11@hotmail.com>
2022-10-10 20:58:36 +0300

    try

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,4 @@
 variable "main_vpc" {
-  description = "main vpc"
-  type = map(object({
-    cidr_blocks = list(string)
-  }))
-  default = {
-    "main" = {
-      cidr_blocks = [ "10.0.0.0/16" ]
-    }
-  }
+    type = set(string)
+    default = [ "10.0.0.0/16" ]
 }
