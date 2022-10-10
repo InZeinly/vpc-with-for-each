346f3b9d813acf8c16bb3bdfe445a9dd7f32f943
InZeinly <tuzar11@hotmail.com>
2022-10-10 20:58:36 +0300

    try

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+    region = "eu-central-1"
+  
+}
+
+
 module "vpc" {
   source = "./modules/vpc"
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,4 @@
 resource "aws_vpc" "main" {
-    dynamic "cidr_block" {
-        for_each = var.main_vpc
-        content{
-            cidr_blocks = main_vpc.value.main
-        }
-    }
+    for_each = var.main_vpc
+    cidr_block = each.value
 }
