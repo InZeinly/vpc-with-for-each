644448f4c380243cbe72b7ad453a53820bdde1c1
InZeinly <tuzar11@hotmail.com>
2022-10-10 22:00:27 +0300

    try

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,7 +7,8 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "privates" {
     vpc_id = aws_vpc.main
-    for_each = each.value["cidr"]
+    for_each = var.subprivate
+    cidr_block = each.value["cidr"]
     tags = each.value["tags"]
   
 }
