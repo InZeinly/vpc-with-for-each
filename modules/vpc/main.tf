a2b9fd41ee887d8864fb630bb3e94c783e6cb749
InZeinly <tuzar11@hotmail.com>
2022-10-10 22:24:27 +0300

    try

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 
 # ask about using other resource variable from for each to another one
 resource "aws_subnet" "privates" {
-    vpc_id = aws_vpc.main["main"]
+    vpc_id = aws_vpc.main.key
     for_each = var.subprivate
     cidr_block = each.value["cidr"]
     tags = each.value["tags"]
