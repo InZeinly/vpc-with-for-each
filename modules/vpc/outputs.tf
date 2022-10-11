71c4023ef9f4de3c77adec864a6934a2ed883553
InZeinly <tuzar11@hotmail.com>
2022-10-11 08:19:40 +0300

    try

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,3 +5,11 @@ data "aws_availability_zones" "available" {
 output "list_of_az" {
   value = data.aws_availability_zones.available[*].names
 }
+
+output "rts" {
+  value = aws_route_table.RT.*.route_table_id
+}
+
+output "subnet" {
+  value = aws_subnet.subnets.*.id
+}
