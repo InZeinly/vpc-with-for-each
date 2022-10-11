93c6172a0a851931615baf90ec07280218ab04d3
InZeinly <tuzar11@hotmail.com>
2022-10-11 08:28:43 +0300

    try

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -59,6 +59,6 @@ resource "aws_route_table" "RT" {
 # RT Association
 resource "aws_route_table_association" "privates_ass" {
   for_each = aws_route_table.RT
-  subnet_id = aws_subnet.subnets.id
-  route_table_id = aws_route_table.RT.id
+  subnet_id = aws_subnet.subnets[*].id
+  route_table_id = aws_route_table.RT["Private*"].id
 }
