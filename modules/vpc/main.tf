f567ed5ab0e42106259129800e0f7ecb5dd83465
InZeinly <tuzar11@hotmail.com>
2022-10-11 08:54:14 +0300

    try

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -52,7 +52,7 @@ resource "aws_route_table" "RT" {
     for_each = var.route-tables
     content {
       cidr_block = route.value.cidr_block
-      gateway_id = each.value.id
+      gateway_id = aws_internet_gateway.igw[*].id
     }
   }
 }
