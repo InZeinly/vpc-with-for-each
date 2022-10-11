8a0aea9440f61d948b73a7d610c8c16449c1e70d
InZeinly <tuzar11@hotmail.com>
2022-10-11 06:18:11 +0300

    try

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "aws" {
     region = var.region
+    provider "aws" {
+    shared_config_files      = ["./.aws/conf"]
+    shared_credentials_files = ["./.aws/creds"]
+    profile                  = "customprofile"
   
 }
 
