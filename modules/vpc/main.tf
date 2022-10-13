d41081243d056d11e6a42ffd93bd651b801cc8e5
InZeinly <tuzar11@hotmail.com>
2022-10-13 07:08:08 +0300

    try

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -44,8 +44,8 @@ resource "aws_nat_gateway" "nat" {
 
 # Route tables
 resource "aws_route_table" "RT" {
-  for_each = var.route-tables
   gateway_id = aws_nat_gateway["nat-gateway"].id
+  for_each = var.route-tables
   tags = each.value["tags"]
   vpc_id = aws_vpc.main["main"].id
 
