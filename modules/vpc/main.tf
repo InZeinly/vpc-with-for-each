9cbd09642f1bc93b812dc27e6f5b4a8c70b6326c
InZeinly <tuzar11@hotmail.com>
2022-10-13 07:07:43 +0300

    try

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,6 +45,7 @@ resource "aws_nat_gateway" "nat" {
 # Route tables
 resource "aws_route_table" "RT" {
   for_each = var.route-tables
+  gateway_id = aws_nat_gateway["nat-gateway"].id
   tags = each.value["tags"]
   vpc_id = aws_vpc.main["main"].id
 
@@ -52,7 +53,6 @@ resource "aws_route_table" "RT" {
     for_each = var.route-tables
     content {
       cidr_block = route.value.cidr_block
-      gateway_id = aws_nat_gateway["nat-gateway"].id
       # gateway_id = each.value.cidr_block # Check
     }
   }
