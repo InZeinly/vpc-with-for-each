7e841a81a9bddb80f108718c5c3294614044af8a
InZeinly <tuzar11@hotmail.com>
2022-10-20 15:48:17 +0300

    added ecs

---
+++ modules/ecs/main.tf
@@ -0,0 +1,88 @@
+resource "aws_ecr_repository" "test_repo" {
+  name                 = "test repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "ecr repo"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_iam_role" "TaskExecRole" {
+  name = "exec role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name = "iam-role"
+    Environment = "Prod"
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+    version = "2012-10-17"
+    statement {
+      action = ["sts:AssumeRole"]
+      effect = "Allow"
+      sid = ""
+
+      principals {
+        type = "Service"
+        identifiers = ["ecs-tasks.amazonaws.com"]
+      }
+    }
+}
+
+resource "aws_iam_role_policy_attachment" "TaskRolePolicy" {
+  role = aws_iam_role.TaskExecRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_ecs_cluster" "test_cluster" {
+  name = "test cluster"
+  tags = {
+    "Name" = "test cluster"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_ecs_task_definition" "task-definition" {
+  family = "some name"
+  execution_role_arn = aws_iam_role.TaskExecRole.arn
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FAGRATE"]
+  cpu = "256"
+  memory = "512"
+  container_definitions = jsonencode([
+    {
+        name = "first"
+        #image = "unknown yet"
+        essential = true
+        portMappings = [
+            {
+                containerPort = 80
+                hostPort = 80
+            }
+        ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs" {
+    name = "ecs service"
+    cluster = aws_ecs_cluster.test_cluster.id
+    task_definition = aws_ecs_task_definition.task-definition.id
+    launch_type = "FARGATE"
+    scheduling_strategy = "REPLICA"
+    desired_count = 1
+    force_new_deployment = true
+    network_configuration {
+      subnets = var.private_subnet_cidr
+      assign_public_ip = true
+    }
+    depends_on = [
+      var.aws_alb_listener , var.iam_role
+    ]
+}
---
+++ modules/ecs/variables.tf
@@ -0,0 +1,5 @@
+variable "private_subnet_cidr" {}
+
+variable "aws_listener" {}
+
+variable "iam_role" {}
