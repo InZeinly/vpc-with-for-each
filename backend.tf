c148fb07398078641fab35e2887e07976d31b69a
InZeinly <tuzar11@hotmail.com>
2022-10-29 13:03:58 +0300

    try

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     encrypt = true
-#     dynamodb_table = "terraform_state_lock"
-#     key    = "terraform.tfstate"
-#     region = "eu-central-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    encrypt = true
+    dynamodb_table = "terraform_state_lock"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
