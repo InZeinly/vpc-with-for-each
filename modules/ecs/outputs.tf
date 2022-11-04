3ee8d7cd5b11cfff099f0ab322e91b8edfe81125
InZeinly <tuzar11@hotmail.com>
2022-11-04 20:18:58 +0200

    try

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,3 +1,8 @@
 output "iam_role" {
   value = aws_iam_role.TaskExecRole.id
 }
+
+output "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -9,3 +9,12 @@ variable "vpc_id" {}
 variable "image_tag" {
     default = "0.0.01"
 }
+
+variable "app_name" {
+  default = "testapp"
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
