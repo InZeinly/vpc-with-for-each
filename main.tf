a263a64becfc008e710cb18b1cfa888d26a61119
InZeinly <tuzar11@hotmail.com>
2022-11-04 20:14:03 +0200

    try

--- main.tf
+++ main.tf
@@ -29,6 +29,10 @@ module "ecs" {
   private_subnet_cidr = module.vpc.private_subnet_id
   alb_listener = module.alb.alb_listener
   iam_role = module.ecs.iam_role
+  image_tag = var.image_tag
+  app_name = "testapp"
+  environment = "testenv"
+  app_count = var.app_count
 }
 
 module "s3_bucket" {
--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -5,3 +5,7 @@ variable "alb_listener" {}
 variable "iam_role" {}
 
 variable "vpc_id" {}
+
+variable "image_tag" {
+    default = "0.0.01"
+}
