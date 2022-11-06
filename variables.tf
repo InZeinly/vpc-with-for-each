647d87b5caff8f37bf7ec17a885281deab7967c8
InZeinly <tuzar11@hotmail.com>
2022-11-06 18:25:25 +0200

    try

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "github_oauth_token" {}
 # variable "vpc_id" {}
 
 variable "private_subnet_id" {
-    value = modules.module.vpc.private_subnet_id
+    values = modules.module.vpc.private_subnet_id
 }
 
 # variable "github_oauth_token" {
