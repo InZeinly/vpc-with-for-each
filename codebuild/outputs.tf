12de9a0ddb89b08af54f10e19d72c36fd3ec96d0
InZeinly <tuzar11@hotmail.com>
2022-11-06 17:25:36 +0200

    try

--- codebuild/outputs.tf
+++ codebuild/outputs.tf
@@ -6,13 +6,13 @@ output "repo_url" {
   value = var.repo_url
 }
 
-# output "priv_subnet" {
-#   value = var.private_subnet_id
-# }
-
 output "priv_subnet" {
-  value = values(aws_subnet.subnets_pub).*.id
+  value = var.private_subnet_id
 }
+
+# output "priv_subnet" {
+#   value = values(aws_subnet.subnets_pub).*.id
+# }
 
 output "COMMIT_MESSAGE" {
   value = var.COMMIT_MESSAGE
