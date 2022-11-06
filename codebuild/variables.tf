24df476c5516ef7a55c987d5941b512a3ae40fb1
InZeinly <tuzar11@hotmail.com>
2022-11-06 17:46:09 +0200

    try

--- codebuild/variables.tf
+++ codebuild/variables.tf
@@ -32,7 +32,7 @@ variable "app_name" {
 
 variable "subnets" {
   type        = list(string)
-  default     = values(aws_subnet.subnets_priv).*.id
+  default     = aws_subnet.subnets_priv.*.id
   description = "The subnet IDs that include resources used by CodeBuild"
 }
 
