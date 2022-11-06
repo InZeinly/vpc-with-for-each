ffe2a72dab54456a9bb4bfaee9cba20f19bcb473
InZeinly <tuzar11@hotmail.com>
2022-11-06 16:47:15 +0200

    try

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "ecs" {
   image_tag = var.image_tag
   app_name = "testapp"
   app_count = 1
-  aws_alb_target_group = module.alb.aws_alb_target_group.app
+  aws_alb_target_group = module.alb.aws_alb_target_group.app.id
 }
 
 module "s3_bucket" {
