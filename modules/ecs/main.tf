e65cf2c73d3695a82f6bb8df6d456e72f0b686aa
InZeinly <tuzar11@hotmail.com>
2022-11-12 20:49:24 +0200

    typo

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -229,7 +229,7 @@ resource "aws_ecs_service" "ecs" {
 
 resource "aws_security_group" "task_sg" {
   name = "task_sg"
-  vpc_id = aws_vpc.main.id
+  vpc_id = var.vpc_id
 
     ingress {
     protocol    = "tcp"
