fcd10aa19e8ec84b59294ff42f87106b3aa8fbcf
InZeinly <tuzar11@hotmail.com>
2022-11-12 22:16:49 +0200

    try

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 #   backend "s3" {
 #     bucket = "inzeintestbucket"
 #     encrypt = true
-#     dynamodb_table = "terraform_state_lock"
+#     # dynamodb_table = "terraform_state_lock"
 #     key    = "terraform.tfstate"
 #     region = "eu-central-1"
 #   }
@@ -10,4 +10,4 @@
 
 # test new branch on virtual
 # test new branch
-#test
+# test
