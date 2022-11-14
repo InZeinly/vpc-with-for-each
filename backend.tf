9f9918c6abd32dfd846bfc9db9f828c94a7abbf4
InZeinly <tuzar11@hotmail.com>
2022-11-14 21:37:15 +0200

    rm sec group

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "inzeintestbucket2198312312"
-#     encrypt = true
-#     # dynamodb_table = "terraform_state_lock"
-#     key    = "terraform.tfstate"
-#     region = "eu-central-1"
-#   }
-# } 
+terraform {
+  backend "s3" {
+    bucket = "inzeintestbucket2198312312"
+    encrypt = true
+    # dynamodb_table = "terraform_state_lock"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+} 
 
 # # test new branch on virtual
 # # test new branch
